0d2b81cdbaceeb7c970fb3541b9ce503ffe63299
jakob <jakob.foeger@volcano.at>
2020-07-30 21:14:07 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 \*#################################################################*/
-
+//test
 #include "main.h"
 
 #include "AppDebug.h"
